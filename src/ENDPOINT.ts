6ccc6fd44245439ae5683fdf05db543526fe4f17
Nathaniel Fernandes <nathaniel.s.fernandes@gmail.com>
2022-05-23 23:57:34 -0400

    linked rim

--- src/ENDPOINT.ts
+++ src/ENDPOINT.ts
@@ -1,1 +1,4 @@
-export const ENDPOINT = "http://localhost:80"
+// export const ENDPOINT = "http://localhost:80"
+export const ENDPOINT = "https://rim.ncp.nathanferns.xyz";
+export const CDN_ENDPOINT = "https://rimu.b-cdn.net";
+
--- src/Gallery.svelte
+++ src/Gallery.svelte
@@ -1,21 +1,23 @@
 <script lang="ts">
-	import Gallery from 'svelte-image-gallery'
-	import axios from 'axios'
-	import { ENDPOINT } from './ENDPOINT'
-	export let token: string;
+  import Gallery from "svelte-image-gallery";
+  import axios from "axios";
+  import { ENDPOINT, CDN_ENDPOINT } from "./ENDPOINT";
+  export let token: string;
 
-
-	const imgs = axios.get(`${ENDPOINT}/list/imgs/${token}`).then(res => console.log(res.data) as any || res.data.filter(e=>e != ".keep"));
-
+  const imgs = axios
+    .get(`${ENDPOINT}/list/imgs/${token}`)
+    .then(
+      (res) =>
+        (console.log(res.data) as any) || res.data.filter((e) => e != ".keep")
+    );
 </script>
 
 {#await imgs}
-    loading...
-{:then res} 
-    
-	<Gallery gap="10" maxColumnWidth="200">
-		{#each res as img}
-			<img src="{ENDPOINT}/imgs/{img}" alt="a"/>
-		{/each}
-	</Gallery>
+  loading...
+{:then res}
+  <Gallery gap="10" maxColumnWidth="200">
+    {#each res as img}
+      <img src="{CDN_ENDPOINT}/imgs/{img}" alt={img} />
+    {/each}
+  </Gallery>
 {/await}
